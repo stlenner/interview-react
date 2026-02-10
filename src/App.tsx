331d74b1a35d07f273bda37442c5ecb186448b7d
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2026-02-10 15:15:23 +0100

    chore: switch to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import {
   RouterProvider,
-  createHashRouter,
+  createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
 import Routes from "./routes/routes";
 import queryClient from "./queryClient";
 
 export default function App() {
-  const router = createHashRouter(createRoutesFromElements(Routes));
+  const router = createBrowserRouter(createRoutesFromElements(Routes));
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
